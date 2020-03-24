460ef20e153208aea3ed8b6cafdccfca77ef0004
José Américo Lacerda Félix <joseamericolf@gmail.com>
2020-03-24 17:23:15 -0300

    separada criação do dat

---
+++ criaDat.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import pickle
+
+# arquivo do banco de dados
+arquivo_dat = 'registro-faces.dat'
+
+# aqui é onde deve buscar o banco de rosto ja guardados
+if os.path.exists(arquivo_dat):
+    print("O arquivo ",arquivo_dat," ja existia")
+    # abrir o arquivo_dat
+    
+    with open(arquivo_dat, 'rb') as f:
+        codificacoesDeFaceConhecidas = pickle.load(f)
+        
+    # preencher as variaveis com os valores do arquivo_dat e
+    # carregar todas as codificações das faces
+
+    # grave uma lista dos nomes e uma das codificações ou inicialize com vazio caso não tenha nada salvo
+    if (len(codificacoesDeFaceConhecidas)>0):
+            
+        nomesDeFaceConhecidas = list(codificacoesDeFaceConhecidas.keys())
+        codificacoesDeFaceConhecidas = list(codificacoesDeFaceConhecidas.values())
+    else:
+        
+        nomesDeFaceConhecidas = [] 
+        codificacoesDeFaceConhecidas = []
+
+else:
+    # caso o arquivo do banco de dados não exista crie e preencha as variaveis com vazio
+    pickle.dump([], open( arquivo_dat, "wb" ))
+    print("O arquivo ",arquivo_dat," não existia e foi criado")
+
+    # abrir o arquivo recém criado que esta vazio
+    with open(arquivo_dat, 'rb') as f:
+        codificacoesDeFaceConhecidas = pickle.load(f)
+        
+    # preencher as variaveis com os valores do arquivo_dat e
+    # carregar todas as codificações das faces
+
+    # grave uma lista dos nomes e uma das codificações
+    nomesDeFaceConhecidas = [] 
+    codificacoesDeFaceConhecidas = []
+print("Nome das faces carregadas do arquivo ",arquivo_dat)
+print(nomesDeFaceConhecidas)
